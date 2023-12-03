bb5cecfa9823ce8ff0a8628a565d9f14d31d5d04
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-03 17:33:41 +0600

    added new function

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The Calculator App',
+      title: 'The App -the first version',
       theme: ThemeData(
         // This is the theme of your application.
         //
