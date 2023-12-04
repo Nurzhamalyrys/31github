d1d1f4ee35240acc5422d6b398c8b7803375122e
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-04 18:53:33 +0600

    added new function

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.yellow,
       ),
       home: const MyHomePage(title: 'The App -the first version'),
     );
@@ -48,7 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 1;
 
   void _incrementCounter() {
     setState(() {
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Push here and it will show here.:',
+              'Push here and it will show here:',
             ),
             Text(
               '$_counter',
