20de42c536134f0cad8f7b7d52b4b95f9a44977d
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-09 13:33:39 +0600

    added new function

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/secondpage.dart';
-import 'package:flutter_application_1/pages/thirdpage.dart';
-
-
 
 void main() {
   runApp(
      MaterialApp(
-      home: Page1(),
-    ),
-  );
-}
-
-class Page1 extends StatelessWidget {
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(_createRoute());
-          },
-          child: const Text('Press!'),
-          
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('New Year Shop'),
+          centerTitle: true
         ),
-      ),
-    );
-  }
-}
 
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
-    },
+        body: Center (
+          child: (Text('Happy New Year')
+          ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Text ('click'),
+          ),
+        
+      ),
+    ),
   );
 }
 
