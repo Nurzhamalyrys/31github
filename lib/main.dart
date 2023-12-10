b0757e47c072416f311541d8600f91484f8c77a5
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-10 10:51:32 +0600

    added new function

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,28 @@ void main() {
      MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('New Year Shop'),
-          centerTitle: true
+          title: const Text(
+            'New Year Shop',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.black
+            ),
+            ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF42A5F5),
         ),
 
         body: Center (
-          child: (Text('Happy New Year')
+          child: (Text('Happy New Year, everyone!')
           ),
           ),
           floatingActionButton: FloatingActionButton(
+            onPressed: () {},
             child: Text ('click'),
+            backgroundColor: Colors.greenAccent,
           ),
-        
       ),
     ),
   );
