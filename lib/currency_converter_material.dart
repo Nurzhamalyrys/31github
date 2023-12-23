5dd5c72b88e32b19ddc2bbc7a8ccb6e42706367f
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-23 18:58:17 +0600

    added new function

--- lib/currency_converter_material.dart
+++ lib/currency_converter_material.dart
@@ -7,7 +7,18 @@ class Currency_converter_MaterialPage  extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text ('Hello World'),
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+          Text('0',
+          style: TextStyle(
+            fontSize: 55,
+            fontWeight: FontWeight.bold,
+            color: Colors.amber
+          ),
+          )
+         ],
+        ),
       ),
       );
     )
