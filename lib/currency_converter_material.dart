db6800180d23a1d343adb8ae10d49ff35cb990c9
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-24 13:16:47 +0600

    added new function

--- lib/currency_converter_material.dart
+++ lib/currency_converter_material.dart
@@ -1,28 +1,37 @@
 
 import 'package:flutter/material.dart';
 
-class Currency_converter_MaterialPage  extends StatelessWidget {
-  
-  @override
+class CurrencyConventerMaterialDart  extends StatelessWidget { 
+   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    var decoration;
+    return  Scaffold(
+      backgroundColor:Colors.amberAccent ,
       body: Center(
         child: Column (
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: (MainAxisAlignment.center),
          children: [
-          Text('0',
+         const  Text(
+            '0',
           style: TextStyle(
             fontSize: 55,
             fontWeight: FontWeight.bold,
             color: Color.fromARGB(255, 255, 255, 255),
           ),
-          decoration:InputDecoration(
+         ),
+         TextField(
+          style: TextStyle(
+            color: Colors.black,
+            ),
+            ),
+          decoration: InputDecoration (
             hintText: 'Please enter the amount in USA',
            textStyle: TextStyle(
             color: Colors.black,
            ),
+          ),
            prefixIcon: Icon(Icons.monetization_on_outlined),
-           prefix:Colors.black,
+           prefix: Colors.black12, 
            filled: true,
            fillColor: Colors.white,
            focusedBorder:OutlineInputBorder(
@@ -32,9 +41,7 @@ class Currency_converter_MaterialPage  extends StatelessWidget {
             ),
              ),
           ),
-          ),
-         ],
         ),
-      ),
-      );
-    )
+      );     
+};
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/currency_converter_material.dart';
 
+
 void main( ) {
   runApp(MyApp ());
 }
@@ -8,8 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
-    home: Currency_converter_MaterialPage,
-    );
+    return  MaterialApp(
+    home: CurrencyConventerMaterialDart),
+    ;
   }
 }
