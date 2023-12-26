a25f1c001c9e757f1ccc54ca9208960b06103409
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-26 11:52:55 +0600

    added new function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 void  main() => runApp(MaterialApp (
-  home: Text ('Hello Hinjas'),
+  home: Scaffold (
+    appBar: AppBar (
+      title: Text ('Hello '),
+      centerTitle: true,
+      ),
+     body: Center (
+      child: Text ('Hello 2'),
+      ),
+      ),
+      floatingActionButton: FloatingActionButton (
+        child: Text('Click'),
+      )
 
-));
+  ),
+
+
+);
 
