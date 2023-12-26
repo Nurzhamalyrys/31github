92d3b68ab44cd3d1106b5433fcc5d95f41ce2b1f
Nurzhamalyrys <nurzhamalyrysbekova@gmail.com>
2023-12-26 10:34:32 +0600

    added new function

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/currency_converter_material.dart';
+void  main() => runApp(MaterialApp (
+  home: Text ('Hello Hinjas'),
 
+));
 
-void main( ) {
-  runApp(MyApp ());
-}
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-    home: CurrencyConventerMaterialDart),
-    ;
-  }
-}
